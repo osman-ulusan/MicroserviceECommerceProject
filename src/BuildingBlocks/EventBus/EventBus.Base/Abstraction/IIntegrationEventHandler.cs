81899a6872f6c0d40c3a3fceb0c78080c8a866db
Osman Ulusan <osmanulusan34@hotmail.com>
2023-06-25 22:00:07 +0300

    Add project files.

---
+++ src/BuildingBlocks/EventBus/EventBus.Base/Abstraction/IIntegrationEventHandler.cs
@@ -0,0 +1,18 @@
+﻿using EventBus.Base.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Base.Abstraction
+{
+    public interface IIntegrationEventHandler<IIntegrationEvent> : IntegrationEventHandler where IIntegrationEvent:IntegrationEvent
+    {
+        Task Handle(IIntegrationEvent @event);
+    }
+
+    public interface IntegrationEventHandler
+    {
+    }
+}
---
+++ src/BuildingBlocks/EventBus/EventBus.Base/SubscriptionInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Base
+{
+    public class SubscriptionInfo
+    {
+        public Type HandlerType { get;}
+
+        public SubscriptionInfo(Type handlerType)
+        {
+            HandlerType = handlerType ?? throw new ArgumentNullException(nameof(handlerType));
+        }
+
+        public static SubscriptionInfo Typed(Type handlerType)
+        {
+            return new SubscriptionInfo(handlerType);
+        }
+
+        // eğer  SubscriptionInfo.Typed(handlerType) ile ulaşılmak istenirsede yukarıdaki gibi SubscriptionInfoya gönderebiliyoruz.
+    }
+}
