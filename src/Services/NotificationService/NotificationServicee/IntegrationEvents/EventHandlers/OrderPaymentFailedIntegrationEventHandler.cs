0e8225a8a613f7c404ec7b15ca1de3a22080adbe
Osman Ulusan <osmanulusan34@hotmail.com>
2023-07-07 13:04:15 +0300

    NotificationService eklendi. Console uygulaması.

---
+++ src/Services/NotificationService/NotificationServicee/IntegrationEvents/EventHandlers/OrderPaymentFailedIntegrationEventHandler.cs
@@ -0,0 +1,30 @@
+﻿using EventBus.Base.Abstraction;
+using Microsoft.Extensions.Logging;
+using NotificationService.IntegrationEvents.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotificationService.IntegrationEvents.EventHandlers
+{
+    public class OrderPaymentFailedIntegrationEventHandler : IIntegrationEventHandler<OrderPaymentFailedIntegrationEvent>
+    {
+        private readonly ILogger<OrderPaymentFailedIntegrationEvent> logger;
+
+        public OrderPaymentFailedIntegrationEventHandler(ILogger<OrderPaymentFailedIntegrationEvent> logger)
+        {
+            this.logger = logger;
+        }
+
+        public Task Handle(OrderPaymentFailedIntegrationEvent @event)
+        {
+            // Send Fail Notification (Sms, Email, Push)
+
+            logger.LogInformation($"Order Payment failed with OrderId: {@event.OrderId}, ErrorMessage: {@event.ErrorMessage}");
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/Services/NotificationService/NotificationServicee/Program.cs
@@ -0,0 +1,53 @@
+﻿using EventBus.Base;
+using EventBus.Base.Abstraction;
+using EventBus.Factory;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NotificationService.IntegrationEvents.EventHandlers;
+using NotificationService.IntegrationEvents.Events;
+
+namespace NotificationService
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceCollection services = new ServiceCollection();
+
+            ConfigureServices(services);
+
+            var sp = services.BuildServiceProvider();
+
+            IEventBus eventBus = sp.GetRequiredService<IEventBus>();
+
+            eventBus.Subscribe<OrderPaymentSuccessIntegrationEvent, OrderPaymentSuccessIntegrationEventHandler>();
+            eventBus.Subscribe<OrderPaymentFailedIntegrationEvent, OrderPaymentFailedIntegrationEventHandler>();
+
+
+            Console.WriteLine("Application is running...");
+
+            Console.ReadLine();
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLogging(configure => configure.AddConsole());
+
+            services.AddTransient<OrderPaymentFailedIntegrationEventHandler>();
+            services.AddTransient<OrderPaymentSuccessIntegrationEventHandler>();
+
+            services.AddSingleton<IEventBus>(sp =>
+            {
+                EventBusConfig config = new()
+                {
+                    ConnectionRetryCount = 5,
+                    EventNameSuffix = "IntegrationEvent",
+                    SubscriberClientAppName = "NotificationService",
+                    EventBusType = EventBusType.RabbitMQ
+                };
+
+                return EventBusFactory.Create(config, sp);
+            });
+        }
+    }
+}
