9d0c33356bedc77b0104c7758465600c94f2811e
Osman Ulusan <osmanulusan34@hotmail.com>
2023-07-09 18:04:03 +0300

    Order Service - Domain işlemleri tamamlandı DDD tasarımına uygun.

---
+++ src/Services/OrderService/OrderService.Domain/AggregateModels/OrderAggregate/Address.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderService.Domain.AggregateModels.OrderAggregate
+{
+    public record Address //: Value Object
+    {
+        public string Street { get; private set; }
+        public string City { get; private set; }
+        public string State { get; private set; }
+        public string Country { get; private set; }
+        public string ZipCode { get; private set; }
+
+        public Address()
+        {
+
+        }
+
+        public Address(string street, string city, string state, string country, string zipCode)
+        {
+            Street = street;
+            City = city;
+            State = state;
+            Country = country;
+            ZipCode = zipCode;
+        }
+
+
+        //protected override IEnumerable<object> GetEqualityComponents()
+        //{
+        //    // using a yield return statement to return each element one at a time
+        //    yield return Street;
+        //    yield return City;
+        //    yield return State;
+        //    yield return Country;
+        //    yield return ZipCode;
+        //}
+
+    }
+}
